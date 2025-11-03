684736a305492bf2a466531c826624ffb37f74f4
Guillermo Morales <morales.guillermo2503@gmail.com>
2025-11-02 22:13:42 -0600

    Resource group deployment

---
+++ variables.tf
@@ -0,0 +1,32 @@
+variable "subscription_id" {
+  description = "The subscription ID for the Azure subscription"
+  type        = string
+}
+
+variable "project" {
+    type= string
+    description = "The name of the project the resource group belongs to"
+    default = "carRental"
+}
+
+variable environment {
+    type = string
+    description = "The environment for the resources"
+    default = "prod"
+}
+
+variable "location" {
+  description = "The Azure region to deploy resources in"
+  type        = string
+  default     = "East US 2"
+}
+
+variable "tags" {
+    description = "A map of tags to apply to a resource group"
+    type        = map(string)
+    default ={
+        environment = "production"
+        date        = "2025-31-10"
+        createdBy   = "Terraform"
+    }
+}
